6d0c0842f18488da5fe5606cb92a5395cef357a3
Jose Lopez Cobano <cobitremolo@gmail.com>
2023-07-17 18:44:45 -0400

    Bootstrap navigation bar

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -7,12 +7,19 @@ from helpers import apology, login_required
 from datetime import datetime
 from markupsafe import escape
 
-
-
 app = Flask(__name__)
+app.static_folder = 'static'
 
 
 @app.route("/")
 def index():
-    return render_template("/index.html")
+    return render_template('/index.html')
+
+@app.route("/dragndrop")
+def dragndrop():
+    
+    return render_template("dragndrop.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
---
+++ static/js/quiz.js
@@ -0,0 +1,16 @@
+// const answers = [{conjugation:"comen", id:"comen"},
+//  {conjugation:"come", id:"come"}, 
+//  {conjugation:"comemos", id:"comemos"}];
+
+//  const blank = document.getElementById('blank')
+//  const choices = document.getElementById('choices')
+
+//  while(answers.length){
+//     const index = Math.floor(Math.random() * answers.length);
+//     const span = document.createElement('span');
+//     span.className = "choice";
+//     span.id = answers[index].conjugation;
+//     span.textContent = answers[index].conjugation
+//     choices.appendChild(span);
+//     answers.splice(index, 1);
+//  }
